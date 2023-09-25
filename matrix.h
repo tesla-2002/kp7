029ffed3eb53d998c37dfd81ce876bd42b532c1f
tesla-2002 <tesla_2002@mail.ru>
2023-09-25 19:18:36 +0300

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -20,15 +20,12 @@ typedef struct {
 vector* vector_create (vector* vec);
 matrix* matrix_create (matrix* mat);
 void vector_pushback (vector* vec, int value);
-//matrix* get_matrix (FILE* input, matrix* mat);
 void vector_print(vector* vec);
 void spmatrix_print (matrix* mat);
 void stdmatrix_print (matrix* mat, int m, int n);
 void vector_free (vector* vec);
 void matrix_free (matrix* mat);
-
-//void transpose();
-//bool is_symmetric();
+matrix* transpose (matrix* mat, int m, int n);
 
 
 #endif
